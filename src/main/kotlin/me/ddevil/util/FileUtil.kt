5757b61c338ae25b6c50ab0f292a7ad2eda745ea
DDevil_ <brunorbsf@gmail.com>
2017-07-10 18:15:42 -0400

    - Renamed File.isEmpty to File.hasChildren because it was misleading

--- src/main/kotlin/me/ddevil/util/FileUtil.kt
+++ src/main/kotlin/me/ddevil/util/FileUtil.kt
@@ -2,5 +2,4 @@ package me.ddevil.util
 
 import java.io.File
 
-fun File.isEmpty() = listFiles().isEmpty()
-fun File.isNotEmpty() = listFiles().isNotEmpty()
+fun File.hasChildren() = listFiles().isEmpty()
