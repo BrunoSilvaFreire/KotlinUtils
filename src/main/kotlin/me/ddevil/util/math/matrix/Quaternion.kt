c8703d022644e3a02ceb0f2f71b481ac4d6c38ea
DDevil_ <brunorbsf@gmail.com>
2017-12-14 16:09:42 -0300

    - Working on Quaternion.kt

--- src/main/kotlin/me/ddevil/util/math/matrix/Quaternion.kt
+++ src/main/kotlin/me/ddevil/util/math/matrix/Quaternion.kt
@@ -4,47 +4,76 @@ import me.ddevil.util.math.*
 import me.ddevil.util.math.vector.Vector3f
 
 
-class Quaternion
+data class Quaternion
 @JvmOverloads
 constructor(
         var x: Float = 0F,
         var y: Float = 0F,
         var z: Float = 0F,
-        var w: Float = 0F
+        var w: Float = 1F
 ) {
     companion object {
-        fun fromEuler(pitch: Float, roll: Float, yaw: Float): Quaternion {
+        val zero = Quaternion()
+        fun fromEulerAngle(x: Float, y: Float, z: Float): Quaternion {
+            return fromEulerAngle(Vector3f(x, y, z))
+        }
+
+        fun fromEulerAngle(angle: Vector3f): Quaternion {
+            val q = Quaternion()
+            q.eulerAngle = angle
+            return q
+        }
+
+        fun fromAxisAngle(axis: Vector3f, angle: Float): Quaternion {
+            return fromAxisAngle(axis.x, axis.y, axis.z, angle)
+        }
+
+        fun fromAxisAngle(ax: Float, ay: Float, az: Float, angle: Float): Quaternion {
+            // w = cos(tetha / 2)
+            // (x,y,z) = v = sin(tetha / 2) * r
+            val qx = ax * sin(angle / 2)
+            val qy = ay * sin(angle / 2)
+            val qz = az * sin(angle / 2)
+            val qw = cos(angle / 2)
+            return Quaternion(qx, qy, qz, qw)
+        }
+    }
+
+    val inverted get() = Quaternion(-x, -y, -z, w)
+
+    var eulerAngle: Vector3f
+        get() {
+            val sinr = 2F * (w * x + y * z)
+            val cosr = 1F - 2F * (x * x + y * y)
+            val roll = atan2(sinr, cosr)
+
+            val sinp = 2F * (w * y - z * x)
+            val pitch = if (fabs(sinp) >= 1) {
+                copysign(PI / 2, sinp)
+            } else {
+                asin(sinp)
+            }
+
+            val siny = 2F * (w * z + x * y)
+            val cosy = 1F - 2F * (y * y + z * z)
+            val yaw = atan2(siny, cosy)
+            return Vector3f(roll, pitch, yaw)
+        }
+        set(value) {
+            val pitch = value.x
+            val roll = value.y
+            val yaw = value.z
             val cy = cos(yaw / 2)
             val sy = sin(yaw / 2)
             val cr = cos(roll / 2)
             val sr = sin(roll / 2)
             val cp = cos(pitch / 2)
             val sp = sin(pitch / 2)
-            val w = cy * cr * cp + sy * sr * sp
-            val x = cy * sr * cp - sy * cr * sp
-            val y = cy * cr * sp + sy * sr * cp
-            val z = sy * cr * cp - cy * sr * sp
-            return Quaternion(x, y, z, w)
+            w = cy * cr * cp + sy * sr * sp
+            x = cy * sr * cp - sy * cr * sp
+            y = cy * cr * sp + sy * sr * cp
+            z = sy * cr * cp - cy * sr * sp
         }
-    }
-
-    fun toEuler(): Vector3f {
-        val sinr = 2F * (w * x + y * z)
-        val cosr = 1F - 2F * (x * x + y * y)
-        val roll = atan2(sinr, cosr)
-
-        val sinp = 2F * (w * y - z * x)
-        val pitch = if (fabs(sinp) >= 1) {
-            copysign(PI / 2, sinp)
-        } else {
-            asin(sinp)
-        }
-
-        val siny = 2F * (w * z + x * y)
-        val cosy = 1F - 2F * (y * y + z * z)
-        val yaw = atan2(siny, cosy)
-        return Vector3f(roll, pitch, yaw)
-    }
 
 
     fun toMatrix(destination: Matrix4) {
