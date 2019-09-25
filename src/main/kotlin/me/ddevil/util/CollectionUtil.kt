c9e50a6678a5fdf1403626d2285bd44ffef34687
BrunoSilvaFreire <brunorbsf@gmail.com>
2019-09-25 09:35:09 -0300

    > Collections util

--- src/main/kotlin/me/ddevil/util/CollectionUtil.kt
+++ src/main/kotlin/me/ddevil/util/CollectionUtil.kt
@@ -4,3 +4,9 @@ fun <C : MutableCollection<T>, T> C.append(value: T): C {
     add(value)
     return this
 }
+
+fun <E> list(builder: ArrayList<E>.() -> Unit): ArrayList<E> {
+    val list = ArrayList<E>()
+    list.builder()
+    return list
+}
