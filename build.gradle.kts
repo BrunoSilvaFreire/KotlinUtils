49ede579ac868d5ed370eea9758b481877a5ea2c
BrunoSilvaFreire <brunorbsf@gmail.com>
2019-10-30 11:04:03 -0300

    > GitHub Packages and organizing gradle

--- build.gradle.kts
+++ build.gradle.kts
@@ -6,16 +6,8 @@ buildscript {
 plugins {
     kotlin("jvm") version "1.3.50"
     id("io.wusa.semver-git-plugin") version "2.0.0-alpha.1"
+    id("com.novoda.build-properties") version "0.4.1"
     `maven-publish`
-}
-
-
-val ver = semver.info
-
-allprojects {
-    group = "me.ddevil"
-    version = ver
-
 }
 
 repositories {
@@ -26,10 +18,59 @@ dependencies {
     compile(kotlin("stdlib-jdk8"))
     compile("com.google.guava:guava:28.1-jre")
 }
+
+val ver = semver.info
+val secretPropertiesName = "secrets"
+buildProperties {
+    create(secretPropertiesName) {
+        using(file("secrets.properties"))
+    }
+}
+
+val (gitHubPackagesUsername, gitHubPackagesToken) = with(buildProperties) {
+    var u: String? = null
+    var p: String? = null
+    named(secretPropertiesName) {
+        u = getAt("gpr.username").string
+        p = getAt("gpr.token").string
+    }
+    return@with u to p
+}
+
+allprojects {
+    group = "me.ddevil"
+    version = ver
+    apply(plugin = "org.gradle.maven-publish")
+
+    publishing {
+        repositories {
+            maven {
+                name = "GitHubPackages"
+                url = uri("https://maven.pkg.github.com/BrunoSilvaFreire/KotlinUtils")
+                credentials {
+                    username = gitHubPackagesUsername
+                    password = gitHubPackagesToken
+                }
+            }
+        }
+    }
+}
+
+
+val sourcesJar by tasks.creating(Jar::class) {
+    group = "documentation"
+    from(sourceSets.main.get().allSource)
+    archiveClassifier.set("sources")
+}
+
 publishing {
     publications {
         create<MavenPublication>("kotlinUtils") {
+            pom {
+                url.set("https://github.com/BrunoSilvaFreire/KotlinUtils")
+            }
             from(components["java"])
+            artifact(sourcesJar)
         }
     }
 }
