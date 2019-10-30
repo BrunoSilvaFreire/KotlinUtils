6d020296e6ae56a44317611b4ee396bea318a005
BrunoSilvaFreire <brunorbsf@gmail.com>
2019-10-30 11:11:45 -0300

    > Including sources jar on JSON

--- json/build.gradle.kts
+++ json/build.gradle.kts
@@ -10,10 +10,16 @@ dependencies {
     testCompile("junit:junit:4.12")
 }
 
+val sourcesJar by tasks.creating(Jar::class) {
+    group = "documentation"
+    from(sourceSets.main.get().allSource)
+    archiveClassifier.set("sources")
+}
 publishing {
     publications {
         create<MavenPublication>("kotlinJson") {
             from(components["java"])
+            artifact(sourcesJar)
         }
     }
 }
